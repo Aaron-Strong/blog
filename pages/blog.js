b4b977fc949d6fcc1aacb5ccdbf7f985cf58e500
Aaron <aaron12-13@hotmail.co.uk>
2017-05-27 12:56:16 +0100

    moo

--- pages/blog.js
+++ pages/blog.js
@@ -1,74 +1,81 @@
-import React from "react"
-import Style from '../components/styles'
-import Link from "next/link"
-import Router from "next/router"
+import React from "react";
+import Style from "../components/styles";
+import Link from "next/link";
+import Router from "next/router";
 export default class extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            flex: "flex",
-            blogPages: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      flex: "flex",
+      blogPages: []
     };
-    componentDidMount() {
-        Router.prefetch('/blog/page')
-    }
-    handleChange = (e) => { 
-        
-    };
-    // Click events for fade out transition
-    // ..whilst keeping next.js Router features
-    handleClickInternal = (e) => { 
-        e.preventDefault();
-        console.log(e.target.href)
-        Router.push({pathname: e.target.href})
-        this.setState( {flex: "flex faded"} )
-    };
-    handleClickExternal = (e) => { 
-        this.setState({flex: "flex faded"})
-    };
-    render() {
-        for(var i = 0; i < 26; i++){
-            this.state.blogPages.push(<div key={i} className="flex-row"><a style={{lineHeight: "50px"}} href={"/" + i} key={i}>{i}</a></div>) 
-        }
-        return (
-            <div>
-                <Style />
-                <div className="bg">
+  }
+  componentDidMount() {
+    Router.prefetch("/blog/page");
+  }
+  handleChange = e => {};
+  // Click events for fade out transition
+  // ..whilst keeping next.js Router features
+  handleClickInternal = e => {
+    e.preventDefault();
+    console.log(e.target.href);
+    Router.push({ pathname: e.target.href });
+    this.setState({ flex: "flex faded" });
+  };
+  handleClickExternal = e => {
+    this.setState({ flex: "flex faded" });
+  };
+  render() {
+    return (
+      <div>
+        <Style />
+        <div className="bg">
 
-                    <div className={this.state.flex}>
+          <div className={this.state.flex}>
 
-                        <div className="flex-blog">
-                            <div style={{borderLeft: "1px dashed white", borderRight: "1px dashed white", width: "20em"}}>
-                                    <div className="flex-row">
-                                        <a onClick={this.handleClickInternal} style={{lineHeight: "50px"}} href="/blog/page" >Hello World</a>
-                                    </div>
-                                    {this.state.blogPages}
-                                <div className="flex-row">
-                                    <p>Soon™</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex-nav">
-                            <div style={{width: "20em"}}>
-                                <div className="vertical-text">
-                                    <div className="flex-row">
-                                        <div className="boxed">
-                                        >/usr/mooku/home/about/
-                                        </div>
-                                        <div className="thirdBoxed">
-                                            <a href="/" onClick={this.handleClickInternal}>/home/</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+            <div className="flex-blog">
+              <div
+                style={{
+                  borderLeft: "1px dashed white",
+                  borderRight: "1px dashed white",
+                  width: "20em",
+                  minHeight: "42em"
+                }}
+              >
+                <div className="flex-row">
+                  <a
+                    onClick={this.handleClickInternal}
+                    style={{ lineHeight: "50px" }}
+                    href="/blog/page"
+                  >
+                    Hello World
+                  </a>
                 </div>
+                {this.state.blogPages}
+                <div className="flex-row">
+                  <p>More Coming Soon™</p>
+                </div>
+              </div>
             </div>
-        )
-    };
-};
+
+            <div className="flex-nav">
+              <div style={{ width: "20em" }}>
+                <div className="vertical-text">
+                  <div className="flex-row">
+                    <div className="boxed">
+                      &gt;/usr/mooku/home/about/
+                    </div>
+                    <div className="thirdBoxed">
+                      <a href="/" onClick={this.handleClickInternal}>/home/</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
--- pages/blog/page.js
+++ pages/blog/page.js
@@ -1,78 +1,82 @@
-import React from "react"
-import Style from '../../components/styles'
-import Link from "next/link"
-import Router from "next/router"
-import {helloWorld} from "../../components/blogPages"
-const HelloWorld = helloWorld.content
-
-
+import React from "react";
+import Style from "../../components/styles";
+import Link from "next/link";
+import Router from "next/router";
+import { helloWorld } from "../../components/blogPages";
+const HelloWorld = helloWorld.content;
 
 export default class extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.state = {
-            flex: "flex",
-            blogPages: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      flex: "flex",
+      blogPages: []
     };
-    componentDidMount() {
-        Router.prefetch('/')
+  }
+  componentDidMount() {
+    Router.prefetch("/");
+    console.log(this.props.url);
+  }
+  handleChange = e => {};
+  // Click events for fade out transition
+  // ..whilst keeping next.js Router features
+  handleClickInternal = e => {
+    e.preventDefault();
+    if (!e.target.href.includes(Router.asPath)) {
+      Router.push({ pathname: e.target.href });
+      this.setState({ flex: "flex faded" });
     }
-    handleChange = (e) => { 
-        
-    };
-    // Click events for fade out transition
-    // ..whilst keeping next.js Router features
-    handleClickInternal = (e) => { 
-        e.preventDefault();
-        if(!e.target.href.includes(Router.asPath)) {
-            Router.push({pathname: e.target.href})
-            this.setState( {flex: "flex faded"} )
-        }
-    };
-    handleClickExternal = (e) => { 
-        this.setState({flex: "flex faded"})
-    };
-    render() {
-        return (
-            <div>
-                <Style />
-                <div className="bg">
+  };
+  handleClickExternal = e => {
+    this.setState({ flex: "flex faded" });
+  };
+  render() {
+    return (
+      <div>
+        <Style />
+        <div className="bg">
 
-                    <div className={this.state.flex}>
-                        <div className="flex-blog">
-                            <div style={{width: "30em"}}>
-                                <div className="flex-row">
-                                    <HelloWorld/>
-                                </div>
-                                <div className="flex-row">
-                                    <a href="/blog" style={{lineHeight: "5em", textDecoration: "underline"}} onClick={this.handleClickInternal}>Go Back</a>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="flex-nav">
-                            <div style={{width: "20em"}}>
-                                <div className="vertical-text">
-                                    <div className="flex-row">
-                                        <div className="boxed">
-                                        >/usr/mooku/home/about/
-                                        </div>
-                                        <div className="thirdBoxed">
-                                            <a href="/" onClick={this.handleClickInternal}>/home/</a>
-                                        </div>
-                                        <div className="thirdBoxed">
-                                            <a href="/blog" onClick={this.handleClickInternal}>/blog/</a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                    </div>
+          <div className={this.state.flex}>
+            <div className="flex-blog">
+              <div style={{ width: "30em" }}>
+                <div className="flex-row">
+                  <HelloWorld />
                 </div>
+                <div className="flex-row">
+                  <a
+                    href="/blog"
+                    style={{ lineHeight: "5em", textDecoration: "underline" }}
+                    onClick={this.handleClickInternal}
+                  >
+                    Go Back
+                  </a>
+                </div>
+              </div>
             </div>
-        )
-    };
-};
+
+            <div className="flex-nav">
+              <div style={{ width: "20em" }}>
+                <div className="vertical-text">
+                  <div className="flex-row">
+                    <div className="boxed">
+                      &gt;/usr/mooku/home/about/
+                    </div>
+                    <div className="thirdBoxed">
+                      <a href="/" onClick={this.handleClickInternal}>/home/</a>
+                    </div>
+                    <div className="thirdBoxed">
+                      <a href="/blog" onClick={this.handleClickInternal}>
+                        /blog/
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
